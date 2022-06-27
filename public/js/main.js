dc7e3c1f2ac23af25bcb7d002671b85e121f74dc
Jay Suresh Bhakte <70802432+jaybhakte@users.noreply.github.com>
2022-06-27 17:12:32 +0530

    weather app updated
    weather app updated

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,7 @@ let cityName = document.getElementById('cityName')
 let city_name = document.getElementById('city_name');
 let temp_status = document.getElementById('temp_status');
 let temp_real_val = document.getElementById('temp_real_val');
+let description = document.getElementById('description');
 
 //function call after click
 let getinfo = async (e) => {
@@ -19,9 +20,12 @@ let getinfo = async (e) => {
             const data = await response.json();
             const arrData = [data]; //array of objects banaya
             city_name.innerText = `${arrData[0].name},${arrData[0].sys.country}`;
-            temp_real_val.innerText = arrData[0].main.temp;
+            const temprature_update = arrData[0].main.temp;
+            temp_real_val.innerText = Math.round(temprature_update/10);
+            // console.log(temp_real_val.innerText);
             const tempMood = arrData[0].weather[0].main;
-
+            description.innerText =  arrData[0].weather[0].description;
+            
             //condition to check sunny or cloudy
             if(tempMood == 'Clear'){
                 temp_status.innerHTML = `<i class="fas fa-cloud-sun" style="color:#eccc68"></i>`
@@ -34,7 +38,7 @@ let getinfo = async (e) => {
             }
             
         } catch {
-            city_name.innerText = 'Please enter the city name properly!';
+            city_name.innerText = 'Please enter the city name properly or kindly check your Internet!';
             temp.innerHTML = '';
             temp_status.innerHTML = '';
         }
